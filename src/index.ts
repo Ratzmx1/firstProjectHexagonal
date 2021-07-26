f7bf7c3db515035f4aab9be086824d51e2ffe68e
Ratzmx <enzoxz.023@gmail.com>
2021-07-25 22:09:39 -0400

    First commit with base structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json);
+
+app.listen(port, () => {
+  console.log(`Listen on port ${port}`);
+});

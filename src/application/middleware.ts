6970cf9db1559ebd913dc56bb4a5a93a8827f7b8
Ratzmx <enzoxz.023@gmail.com>
2021-08-02 23:52:29 -0400

    Renamed folders

---
+++ src/application/middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { middleware } from "../domain/services";
+// import { Request } from "../core/entities/requestAuthorized";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const bearer = req.header("authorization");
+  if (!bearer) {
+    return res.status(400).json({ message: "No token" });
+  }
+  const user = await middleware(bearer);
+  if (!user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.locals.user = user;
+  return next();
+};
---
+++ src/application/userControllers/login.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { loginUser } from "../../domain/services";
+
+export default async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const response = await loginUser(email, password);
+
+    if (!response) {
+      return res.status(400).json({ message: "Email or password incorrect" });
+    }
+
+    return res.json({ user: response?.user, token: response?.token });
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: e.message });
+  }
+};
